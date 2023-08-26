afdc021076618fa93c33268f4037cd7a0608d6c1
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-08-26 11:37:46 +0330

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="container p-8">
+          <div className='max-w-max'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ipsam autem quisquam voluptate aut repudiandae tempore quaerat odio dolore illum. Illum delectus quos atque nihil quo dolorum. Dolorem quasi voluptatibus blanditiis deleniti incidunt maiores iure alias, omnis voluptas odit aliquid!
+          </div>
+    </div>
+  )
+}
